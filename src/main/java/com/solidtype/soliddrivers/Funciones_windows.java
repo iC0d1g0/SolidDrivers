34f1ea0568fc05f1f90d97706544ceebd508ffd4
Adderlis <adderlis@yahoo.com>
2025-02-11 23:34:40 -0400

    Corregi un delate que habia considerable cuando se queria leer las informacion de la pc local

--- src/main/java/com/solidtype/soliddrivers/Funciones_windows.java
+++ src/main/java/com/solidtype/soliddrivers/Funciones_windows.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.solidtype.soliddrivers;
 import com.solidtype.soliddrivers.logicaI_instalador.DriversManager;
 import java.awt.TextArea;
